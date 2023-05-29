93fa18aee5b87cbf3ee9d516e752293e61212736
pranish <pranishlama17@gmail.com>
2023-05-29 15:43:43 +0545

    fixed errors

---
+++ custom/om_hospital/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Hospital Management System',
+    'author': 'Odoo Mates',
+    'website': 'www.odoo.com',
+    'summary': 'Odoo 16 development',
+    'data': [
+        'views/menu.xml',
+        'views/patient.xml',
+        'security/ir.model.access.csv'
+    ]
+}
