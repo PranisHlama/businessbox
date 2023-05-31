731264d374f7311826fb92a7f68800467e28194a
pranish <pranishlama17@gmail.com>
2023-05-31 21:40:00 +0545

    just files

--- custom/om_hospital/__manifest__.py
+++ custom/om_hospital/__manifest__.py
@@ -8,6 +8,7 @@
         'security/ir.model.access.csv',
         'data/sequence.xml',
         'views/menu.xml',
-        'views/patient.xml'
+        'views/patient.xml',
+        'views/doctor.xml'
     ]
 }
