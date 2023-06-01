3c4a8b65c3d52720f7e6b6cefdda3477e0aaf9b4
pranish <pranishlama17@gmail.com>
2023-06-01 13:14:30 +0545

    added all the files for revert

---
+++ custom/om_hospital/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Hospital Management System',
+    'author': 'Odoo Mates',
+    'website': 'www.odoo.com',
+    'summary': 'Odoo 16 development',
+    'depends': ['mail'],
+    'data': [
+        'views/menu.xml',
+        'views/patient.xml',
+        'security/ir.model.access.csv'
+    ]
+}
