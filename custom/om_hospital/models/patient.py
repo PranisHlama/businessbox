3d25a9fc4dc1779a21b4434b6fe76052d1870c58
pranish <pranishlama17@gmail.com>
2023-06-01 13:08:26 +0545

    just

---
+++ custom/om_hospital/models/patient.py
@@ -0,0 +1,40 @@
+from importlib.resources import _
+
+from odoo import api, fields, models
+
+
+
+
+class HospitalPatient(models.Model):
+    _name = "hospital.patient"
+    _inherit = ['mail.thread']
+
+    _description = "patient records"
+    name = fields.Char(string='Name', required=True, tracking=True)
+    age = fields.Integer(string="Age", tracking=True)
+    is_child = fields.Boolean(string="Is Child ?", tracking=True)
+    notes = fields.Text(string="Notes", tracking=True)
+    gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string="Gender")
+
+    capitalized_name = fields.Char(string='Capitalized Name', compute='_compute_capitalized_name', store = True)
+    ref = fields.Char(string="Reference", default=lambda self: _('New'))
+    @api.model_create_multi
+    def create (self, vals_list):
+        for vals in vals_list:
+            vals['ref'] = self.env['ir.sequence'].next_by_code('Hospital.patient')
+        return super(HospitalPatient, self).create(vals_list)
+
+
+    @api.depends('name')
+    def _compute_capitalized_name(self):
+        if self.name:
+            self.capitalized_name = self.name.upper()
+        else:
+            self.capitalized_name = ''
+
+@api.onchange('age')
+def _onchange_age(self):
+    if self.age <= 10:
+        self.is_child = True
+    else:
+        self.is_child = False
