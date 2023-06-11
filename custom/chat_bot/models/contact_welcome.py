3f490dcffce27783e408ccc7dae0fe8e3ebf1270
pranish <pranishlama21@gmail.com>
2023-06-11 21:14:05 +0545

    fixed error

--- custom/chat_bot/models/contact_welcome.py
+++ custom/chat_bot/models/contact_welcome.py
@@ -1,34 +1,48 @@
 from odoo import api, models
+import openai
+
+openai.api_key = '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'
 
 
 class Welcome(models.Model):
     _inherit = 'res.partner'
 
-    # Create Method
-    @api.model
-    def create(self, vals):
-        print("Odoo Mates", vals)
-        vals['website'] = 'TEST.com'
-        new_contact = super(Welcome, self).create(vals)
-        return new_contact
-
-    # OpenAI message
-    def create_message(name):
-        print('create message')
-        message = f"Dear {name},\n\nWelcome to our platform. We are excited to have you on board"
+    def create_message(self, name):
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt="Write a welcome letter to a new {name} at a company",
+            max_tokens=100
+        )
+        message = response.choices[0].text.strip()
         return message
 
-    # WhatsApp
-    def send_message(name, phone_no, email):
-        print('send message')
-        print(name)
-        print(phone_no)
-        print(email)
+    # Create Method
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            message = self.create_message(vals['name'])
+            self.send_message(message, 94857738)
+            new_contact = super(Welcome, self).create(vals)
+            return new_contact
 
-    def user_info(self,vals):
+    # OpenAI message
+
+    def send_message(self,message, phone_no):
+        print(message)
+        print(phone_no)
+        return True
+
+    def user_info(self, vals):
         print("Info done")
         name = vals.get('name', False)
         email = vals.get('email', False)
         phone = vals.get('phone', False)
-        self.create_message(name)
-        self.send_message(name, phone, email)
+        create_message()
+        send_message(name, phone, email)
+
+    # WhatsApp
+    # def whatsapp(self, vals_list):
+    #     phone = create_message(vals['phone'])
+    #     self.send_message(phone, vals['phone'])
+    #     new_contact = super(Welcome, self).create(vals)
+    #     return new_contact
