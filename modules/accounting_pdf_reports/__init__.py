feceb8b4da588557c741884b2a9c3f19c084ee82
pranish <pranishlama21@gmail.com>
2023-06-16 15:46:32 +0545

    Revert "done"
    This reverts commit a5ca75c58b21520670964c8a9ef2d07653f43259.

--- modules/accounting_pdf_reports/__init__.py
+++ modules/accounting_pdf_reports/__init__.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
 
-from . import wizard
-from . import models
-from . import report
-
-
-def _pre_init_clean_m2m_models(cr):
-    cr.execute("""DROP TABLE IF EXISTS account_journal_account_report_partner_ledger_rel""")
+from . import wizards
+from . import reports
